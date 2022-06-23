b7b39701389faf6f715ce7af327ab69802b2562e
MSzarkowicz <u1872347@unimail.hud.ac.uk>
2022-06-24 00:21:19 +0100

    Remove restriction 'unique title'

--- database/migrations/2022_06_17_122619_create_recipes_table.php
+++ database/migrations/2022_06_17_122619_create_recipes_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('recipes', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained();
-            $table->string('title')->unique();
+            $table->string('title');
             $table->text('body');
             $table->integer('rating')->default(5);
             $table->integer('prep_time_in_min')->default(30);
